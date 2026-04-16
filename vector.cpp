f651a215980cf726b62677b006775540ed5db214
laloochoajavier-sudo <laloochoajavier@gmail.com>
2026-04-16 17:33:50 -0600

    segundo paso de los vectores

--- vector.cpp
+++ vector.cpp
@@ -4,6 +4,19 @@
 //tiene una sintaxis mas sencilla que el array
 //proporciona funciones para agregar, eliminar y acceder a los elementos de forma sencilla
 //tambien se puede usar en punteros 
+//tienen un tamaño dinámico, lo que significa que pueden crecer o reducirse según sea necesario
+//los elementos en el interior son del mismo tipo lo que permite un acceso eficiente a los datos
+//se accede a los elementos mediante índices, al igual que con los arrays
+//el primer elemento es indice 0, el segundo es índice 1, y así sucesivamente
+//el ultimo elemento es indice n-1, donde n es el número total de elementos en el vector
+//se suelene usar de los vectores usando bucles para recorrerlos y realizar operaciones en cada elemento
 #include <iostream>
 #include <vector>
-y
+int main(){
+std::vector<int> records(10); //declaramos un vector de enteros llamado records
+std::vector<char> letras(10); //declaramos un vector de caracteres llamado letras
+for(int i=0; i<10; i++){
+    std::cout<<records[i]<<'\n';
+}
+return 0;
+}
