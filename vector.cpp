cf9ef688757dd55b03e5b2d78735873589439e9a
laloochoajavier-sudo <laloochoajavier@gmail.com>
2026-04-16 17:00:06 -0600

    primer paso

---
+++ vector.cpp
@@ -0,0 +1,8 @@
+//vector: un arreglo muy similar sirve para almacenar datos
+//pueden crecer o defcrecer de forma dinamica
+//la diferencia con el array es que hay que prefijar el tamaño del array, mientras que el vector no necesita eso
+//tiene una sintaxis mas sencilla que el array
+//proporciona funciones para agregar, eliminar y acceder a los elementos de forma sencilla
+//tambien se puede usar en punteros 
+#include <iostream>
+#include <vector>
